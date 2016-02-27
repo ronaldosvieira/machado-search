5f552c8c4631453e7925de43a8b5a43af8d5c569
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-02-27 12:28:37 -0300

    Criação da matriz gênero-documentos

--- questions.py
+++ questions.py
@@ -1,3 +1,7 @@
 import machado, numpy as np
 
 metadata_dict = machado.load()
+
+genres = list(metadata_dict.keys())
+
+files_matrix = np.array([np.array([doc['file'] for doc in metadata_dict[genre]]) for genre in genres])
