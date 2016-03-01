73256cde18b74db75a8b0daf0137d060a4ac5be6
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-03-01 10:04:55 -0300

    Mediana e desvio padrão de palavras por gênero

--- questions.py
+++ questions.py
@@ -1,4 +1,4 @@
-import machado, numpy as np
+import machado, numpy as np, matplotlib.pyplot as plt
 
 metadata_dict = machado.load()
 
@@ -38,10 +38,12 @@ print()
 amount_words_matrix = np.array([np.array([len(doc) for doc in genre]) for genre in split_files_matrix])
 
 print("Quantas palavras aparecem em cada documento?")
-print("R: ")# + str(dict(zip(genres, amount_words_matrix))))
+print("R: <resposta grande - descomentar>")# + str(dict(zip(genres, amount_words_matrix))))
 
 print()
 
 print("O tamanho das palavras varia muito?")
 print("R: ")
-print("Média: " + str(dict(zip(genres, map(np.mean, amount_words_matrix)))))
+print("Média: " + str(dict(zip(genres, map(np.round, map(np.mean, amount_words_matrix))))))
+print("Mediana: " + str(dict(zip(genres, map(np.median, amount_words_matrix)))))
+print("Desvio padrao: " + str(dict(zip(genres, map(np.round, map(np.std, amount_words_matrix))))))
