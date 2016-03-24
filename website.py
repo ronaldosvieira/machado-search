5ad77ea420302b0518eb123732bbbbb70830b6e2
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-03-24 20:22:40 -0300

    Integração da busca por múltiplos campos no website

--- website.py
+++ website.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     query = request.args.get('query')
-    fields = ['content', 'title']
+    fields = request.args.getlist('fields')
     
     if query is None:
         return render_template('index.html')
