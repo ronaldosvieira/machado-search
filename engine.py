1753408bfcc8a70afa7f0fe8fc70cdb048410530
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-03-25 13:39:14 -0300

    Correção do schema

--- engine.py
+++ engine.py
@@ -14,7 +14,7 @@ machado_data = machado.load()
 
 my_analyzer = RegexTokenizer() | LowercaseFilter() | StopFilter(lang='portuguese') | CharsetFilter(accent_map)
 
-schema = Schema(title=TEXT(stored=True, field_boost=2.0), content=TEXT(analyzer=my_analyzer), genre=TEXT(stored=True), filepath=ID(stored=True), pdf=TEXT(stored=True), html=TEXT(stored=True))
+schema = Schema(title=TEXT(stored=True, field_boost=2.0, analyzer=my_analyzer), content=TEXT(analyzer=my_analyzer), genre=TEXT(stored=True), filepath=ID(stored=True), pdf=TEXT(stored=True), html=TEXT(stored=True))
 
 if not os.path.exists("whoosh_index"):
     os.mkdir("whoosh_index")
